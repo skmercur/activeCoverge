fc180bb0e20759b3e2f759d6b026868761d29223
sofiane khoudour <khoudoursofiane75@gmail.com>
2020-01-21 20:43:21 +0100

    working version 1

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-import { Upload, Icon, Layout } from "antd";
+import { Upload, Icon, Layout, Tabs, Table } from "antd";
 import readXlsxFile from "read-excel-file";
 import "antd/dist/antd.css";
 export default class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      Selsman: [],
+      Total: [],
+      DispProd: []
+    };
   }
   handleFileUpload = async info => {
     let Selsman = [];
@@ -19,6 +24,7 @@ export default class Main extends Component {
         }
       });
       Selsman = [...new Set(Selsman)];
+      this.setState({ Selsman: Selsman });
       rows.forEach(row => {
         if (row[8] !== null && row[8] !== "Product Description") {
           Products.push(row[8]);
@@ -36,28 +42,86 @@ export default class Main extends Component {
           product: newProd
         });
       });
-      console.log(TotalSells);
 
-      rows.forEach(row => {
+      /**   rows.forEach(row => {
         TotalSells.forEach(data => {
-          data.product.forEach(produit => {
-            let k = 0;
-            if (produit.product === row[8] && data.seller === row[10]) {
+          if (data.seller === row[10]) {
+            data.product.forEach(produit => {
+              let k = 0;
+              if (produit.product === row[8] && data.seller === row[10]) {
+                let qty = row[25];
+                let conversion = row[18];
+                k = k + qty / conversion;
+                produit.qty = k;
+              }
+            });
+          }
+        });
+      });
+      */
+      let tempProduct = [];
+      let TempTot = [];
+      let tempClient = [];
+      Selsman.forEach(seller => {
+        Products.forEach(product => {
+          let k = 0;
+          rows.forEach(row => {
+            if (row[8] === product && row[10] === seller) {
               let qty = row[25];
               let conversion = row[18];
               k = k + qty / conversion;
-              produit.qty = k;
+              tempClient.push(row[1]);
             }
           });
+          tempClient = [...new Set(tempClient)];
+          let NbrClient = tempClient.length;
+          tempClient = [];
+          tempProduct.push({
+            produit: product,
+            qty: k,
+            ActiveCoverge: NbrClient
+          });
         });
+        TempTot.push({
+          Seller: seller,
+          Product: tempProduct
+        });
+        tempProduct = [];
       });
-      console.log(TotalSells);
+      console.log(TempTot);
+      this.setState({ Total: TempTot });
     });
+  };
+  callback = key => {
+    let temp = this.state.Total;
+    let index = temp.findIndex(elem => elem.Seller === key);
+    let prod = this.state.Total[index].Product;
+    this.setState({ DispProd: prod });
   };
   render() {
     const { Header, Footer, Sider, Content } = Layout;
 
     const { Dragger } = Upload;
+    const { TabPane } = Tabs;
+    const columns = [
+      {
+        title: "Product",
+        dataIndex: "produit",
+        sorter: true,
+        key: "produit"
+      },
+      {
+        title: "Quantity",
+        dataIndex: "qty",
+        key: "qty"
+      },
+      {
+        title: "Active Coverge",
+        dataIndex: "ActiveCoverge",
+        key: "ActiveCoverge"
+      }
+    ];
+
     return (
       <>
         <Layout>
@@ -80,6 +144,12 @@ export default class Main extends Component {
               <p className="ant-upload-hint"></p>
             </Dragger>
           </Content>
+          <Tabs defaultActiveKey="1" onChange={this.callback}>
+            {this.state.Selsman.map(i => (
+              <TabPane tab={i} key={i}></TabPane>
+            ))}
+          </Tabs>
+          <Table columns={columns} dataSource={this.state.DispProd} />
         </Layout>
       </>
     );
