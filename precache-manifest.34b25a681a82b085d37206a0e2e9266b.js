04a06d3e5fb6cd2d3d168fb4d35ba4ceae3c6d89
sofiane khoudour <khoudoursofiane75@gmail.com>
2020-01-24 23:11:21 +0100

    Updates

---
+++ precache-manifest.34b25a681a82b085d37206a0e2e9266b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef5ccea952193e382b72fb0304b9fba",
+    "url": "/activeCoverge/index.html"
+  },
+  {
+    "revision": "e607dd2cc0ab127927f6",
+    "url": "/activeCoverge/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "1429e0dac0f53cc26a01",
+    "url": "/activeCoverge/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e607dd2cc0ab127927f6",
+    "url": "/activeCoverge/static/js/2.45660043.chunk.js"
+  },
+  {
+    "revision": "5816dddab24ad62b87c99ef7f5c10f17",
+    "url": "/activeCoverge/static/js/2.45660043.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1429e0dac0f53cc26a01",
+    "url": "/activeCoverge/static/js/main.51ab71cb.chunk.js"
+  },
+  {
+    "revision": "603cb521a72b5d8349c4",
+    "url": "/activeCoverge/static/js/runtime-main.9808ee21.js"
+  }
+]);
